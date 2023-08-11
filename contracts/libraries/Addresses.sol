f83bcbc833cf0c8bb7273d4c48903612c3d9f726
CryptJS13 <jasper@slimmens.nl>
2023-08-11 20:42:53 +0100

    Add and deploy baseswap dex

--- contracts/libraries/Addresses.sol
+++ contracts/libraries/Addresses.sol
@@ -3,4 +3,5 @@ pragma solidity 0.8.17;
 
 library Addresses {
     address constant balancerVault = 0xBA12222222228d8Ba445958a75a0704d566BF2C8;
+    address constant baseSwapRouter = 0x327Df1E6de05895d2ab08513aaDD9313Fe505d86;
 }
